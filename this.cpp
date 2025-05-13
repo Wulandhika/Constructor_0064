5f480a6892ac2679f0ca197d353e2dcf7864cc9d
Wulandhika <wulandhikakurnaliawati@gmail.com>
2025-05-13 16:25:05 +0700

    Membuat this

---
+++ this.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class buku{
+    string judul;
+
+    public:
+    string setget(string judul){
+        this->judul = judul; // nilai parameter 'judul' untuk membuat variable 'judul'
+        return this->judul; // return variable judul.
+    }
+};
+
+int main()
+{
+    buku bukunya;
+    cout << bukunya.setget("Matematika");
+    return 0;
+}
