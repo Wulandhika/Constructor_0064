3e87fdd2f62539814cc591c249e8831a57b6cc3a
Wulandhika <wulandhikakurnaliawati@gmail.com>
2025-05-13 16:35:59 +0700

    Membuat bagian chain function call

--- chainFunctionCall.cpp
+++ chainFunctionCall.cpp
@@ -6,7 +6,7 @@ class buku{
     public:
     buku setJudul(string judul){
         this->judul = judul;
-        return *this; // chain function
+        return *this; // Chain function
     }
     string getJudul(){
         return this->judul;
