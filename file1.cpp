7abec2559131d751455aa82155864451d701e624
Wulandhika <wulandhikakurnaliawati@gmail.com>
2025-05-13 15:43:09 +0700

    Lanjutan membuat file 1

--- file1.cpp
+++ file1.cpp
@@ -1,3 +1,10 @@
 #include <iostream>
 using namespace std;
 
+class BangunDatar;
+class PersegiPanjang{
+    public:
+       void inputData(BangunDatar &bd);
+       void outputData(BangunDatar &bd);
+};
+
