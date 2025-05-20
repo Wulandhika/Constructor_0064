60176160200b779aa5a07d1eca95dee7bb2df873
Wulandhika <wulandhikakurnaliawati@gmail.com>
2025-05-20 15:43:45 +0700

    Mengerjakan Latihan soal lanjutan

--- Latihan.cpp
+++ Latihan.cpp
@@ -15,3 +15,20 @@ public:
         kodeBarang = kode;
     }
 
+    void tampilkanData()
+    {
+        cout << "Data Barang:" << endl;
+        cout << "Nama Barang: " << namaBarang << endl;
+        cout << "Kode Barang: " << kodeBarang << endl;
+    }
+};
+
+int main()
+{
+    Barang barang1("Tas Apip", "12345");
+    Barang barang2("Tas Saya", "10000");
+    barang1.tampilkanData();
+    barang2.tampilkanData();
+
+    return 0;
+}
